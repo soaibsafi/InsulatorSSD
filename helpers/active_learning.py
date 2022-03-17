f5dc2c85c9e583b9d4f224637333f9f6cd18aebd
sooaaib <soaib.safi@gmail.com>
2022-03-17 10:12:33 +0100

    :sparkles:update active learning script

--- helpers/active_learning.py
+++ helpers/active_learning.py
@@ -28,12 +28,15 @@ def get_total_images(path):
     return count
 
 def split_ratio(selected_for_training):
+    previous_files = read_train_test()
+
     print("-------------------------")
     files = []
     fn=[os.path.splitext(filename)[0] for filename in os.listdir(GOOD_CONF_DIR)]
     fn = list(set(fn))
     for f in fn:
         files.append(GOOD_CONF_DIR+f)
+    files = set(files).difference(previous_files)
     good_conf_ratio = int(selected_for_training * 0.45)
     good_conf_files = random.sample(files, good_conf_ratio)
     print("Good Confidence: ", len(good_conf_files))
@@ -44,6 +47,7 @@ def split_ratio(selected_for_training):
     fn = list(set(fn))
     for f in fn:
         files.append(AVERAGE_CONF_DIR+f)
+    files = set(files).difference(previous_files)
     avg_conf_ratio = int(selected_for_training * 0.30)
     avg_conf_files = random.sample(files, avg_conf_ratio)
     print("Average Confidence: ", len(avg_conf_files))
@@ -54,6 +58,7 @@ def split_ratio(selected_for_training):
     fn = list(set(fn))
     for f in fn:
         files.append(BAD_CONF_DIR+f)
+    files = set(files).difference(previous_files)
     bad_conf_ratio = int(selected_for_training * 0.15)
     bad_conf_files = random.sample(files, bad_conf_ratio)
     print("Bad Confidence: ", len(bad_conf_files))
@@ -63,6 +68,7 @@ def split_ratio(selected_for_training):
     fn = list(set(fn))
     for f in fn:
         files.append(LAB_CONF_DIR+f)
+    files = set(files).difference(previous_files)
     lab_conf_ratio = int(selected_for_training * 0.10)
     lab_conf_files = random.sample(files, lab_conf_ratio)
     print("Lab Confidence: ", len(lab_conf_files))
@@ -79,10 +85,9 @@ def write_selected_files(train_fileames, val_fileames, test_fileames):
             f.write("%s\n" % name)
     with open(ROOT_DIR +'test_'+timestr+'.txt', 'w') as f:
         for name in test_fileames:
-            f.write("%s\n" % name)
-        
+            f.write("%s\n" % name)        
 
-def copy_selected_data():
+def read_train_test():
     trainfile_list = glob.glob(os.path.join(ROOT_DIR, 'train_*.txt'))
     trainfiles = []
     for file in trainfile_list:
@@ -101,16 +106,18 @@ def copy_selected_data():
         with open(file, 'r') as f:
             testfiles += f.read().splitlines()
 
-    for item in trainfiles:
+    return trainfiles+validationfiles+testfiles
+
+def copy_selected_data(train_fileames, val_fileames, test_fileames):
+    for item in train_fileames:
             shutil.move(item + '.jpg', ROOT_DIR +'train/')
             shutil.move(item + '.xml', ROOT_DIR +'train/')
-    for item in validationfiles:
+    for item in val_fileames:
             shutil.move(item + '.jpg', ROOT_DIR +'validation/')
             shutil.move(item + '.xml', ROOT_DIR +'validation/')
-    for item in testfiles:
+    for item in test_fileames:
             shutil.move(item + '.jpg', ROOT_DIR +'test/')
             shutil.move(item + '.xml', ROOT_DIR +'test/')
-
 
 def train_test_split(selected_files):
     os.makedirs(ROOT_DIR +'train/', exist_ok=True)
@@ -131,6 +138,7 @@ def train_test_split(selected_files):
     print('Testing: ', len(test_FileNames))
 
     write_selected_files(train_FileNames, val_FileNames, test_FileNames)
+    return train_FileNames, val_FileNames, test_FileNames
 
 
 def main():
@@ -170,8 +178,8 @@ def main():
     print("Selected for Training: ",selected_for_training)
 
     selected_files = split_ratio(selected_for_training)
-    train_test_split(selected_files)
-    copy_selected_data()
+    train_fileames, val_fileames, test_fileames = train_test_split(selected_files)
+    copy_selected_data(train_fileames, val_fileames, test_fileames)
 
     print("-------------------------")
     print("Split Successful. Current details-")
@@ -179,7 +187,6 @@ def main():
     print('Validation: ', get_total_images(ROOT_DIR +'validation/'))
     print('Testing: ', get_total_images(ROOT_DIR +'test/'))
     print("-------------------------")
-
 
 if __name__ == "__main__":
     main()
